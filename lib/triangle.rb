1b161f3e2a670e7a31dfcc476e43beada574200f
Travis Clarkson <tk1667@gmail.com>
2018-02-27 02:30:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @a = side1
+    @b = side2
+    @c = side3
+    @sides = [side1, side2, side3].sort
+
+  end
+
+  def kind
+
+    if @sides.any? { |s| 0 >= s }
+      raise TriangleError
+    elsif @sides[2] >= (@sides[0] + @sides[1])
+      raise TriangleError
+    elsif @sides.any? { |s| @sides.count(s) == 3 }
+      :equilateral
+    elsif @sides.any? { |s| @sides.count(s) == 2 }
+      :isosceles
+    elsif @sides.all? { |s| @sides.count(s) == 1 }
+      :scalene
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
