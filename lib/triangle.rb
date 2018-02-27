35ee56d3f7469666544d07b072670edf209e04e3
Travis Clarkson <tk1667@gmail.com>
2018-02-27 01:34:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Triangle
 
   def initialize(side1, side2, side3)
@@ -5,12 +7,13 @@ class Triangle
     # @side2 = side2
     # @side3 = side3
     @sides = [side1, side2, side3]
+    binding.pry
   end
 
   def kind
 
     case @sides
-    when @sides.count{|s| s == @sides[0]} == 3
+    when @sides.count{|s| s == @sides[0]} == 3 
       :equilateral
     end
 
